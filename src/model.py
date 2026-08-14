b1ab88ef7439432f41b411790643a8988b370750
Supratim_Chatterjee <suparanrocks@gmail.com>
2026-08-15 01:19:34 +0530

    fixed unnecessary download of resnet model when a trained model is present

--- src/model.py
+++ src/model.py
@@ -2,9 +2,9 @@ import torch
 import torch.nn as nn
 from torchvision.models import resnet18, ResNet18_Weights
 
-def get_model(architecture: str="resnet18", num_classes: int=10)-> nn.Module:
+def get_model(architecture: str="resnet18", num_classes: int=10, pretrained: bool=True)-> nn.Module:
     if architecture.lower()=="resnet18":
-        weights=ResNet18_Weights.DEFAULT
+        weights=ResNet18_Weights.DEFAULT if pretrained else None
         model=resnet18(weights=weights)
         #Adapt first conv layer for 32*32 images (CIFAR-10) to avoid over-downsampling
         model.conv1=nn.Conv2d(3,64,kernel_size=3,stride=1,padding=1,bias=False)
